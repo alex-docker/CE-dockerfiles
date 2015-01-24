54643f3316026807ca45c03f9024726a5d49c759
docker <docker@onztv.com>
2015-01-24 21:48:03 +0000

    updated

--- base/docker-softether-2/build.sh
+++ base/docker-softether-2/build.sh
@@ -1,1 +1,5 @@
 docker build -t clouderg/softether-2 .
+
+#docker run -d --name softether -p 1194:1194 clouderg/softether-2
+#docker run -t -i --rm --link softether:softether clouderg/softether-2 vpnadmin
+#docker run -t -i --rm --link softether:softether clouderg/softether-2 bash
